775dffe1880ab0f2131f939196468d4f1e27ea9d
SEP5155 <serg.papusha@gmail.com>
2024-10-07 23:05:12 +0300

    added modal window

---
+++ assets/js/custom.js
@@ -0,0 +1,7 @@
+setTimeout(() => {
+    document.querySelector('modal').classList.add('active');
+}, 2000)
+
+setTimeout(() => {
+    document.querySelector('modal').classList.remove('active');
+}, 8000)
